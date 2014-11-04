7ef25c6ad1be9d04cbb06c8a7d4f80decd7382fa
Alexius Lee <lee.alexorz@gmail.com>
2014-11-04 15:50:30 +0800

    Change to pure express server.

--- server/index.js
+++ server/index.js
@@ -1,41 +1,41 @@
 /**
  *      Express Server
  */
-// var express = require('express');
-// var app = express();
-// var server = require('http').Server(app);
-//
-// app.use(function staticsPlaceholder(req, res, next) {
-//   return next();
-// });
-//
-// exports = module.exports = app;
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+
+app.use(function staticsPlaceholder(req, res, next) {
+  return next();
+});
+
+exports = module.exports = app;
 
 
 /**
  *      Express & Socket Server
  */
-var express = require('express');
-var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-
-// Socket server
-var imageStream = io
-    .of('/image-stream')
-    .on('connection', function (socket) {
-      socket.emit('news', { hello: 'world' });
-      socket.on('my other event', function (data) {
-        console.log(data);
-      });
-    });
-
-exports = module.exports = server;
-
-// marker for `grunt-express` to inject static folder/contents
-server.use = function(){
-    app.use.apply(app, arguments);
-};
-app.use(function staticsPlaceholder(req, res, next) {
-  return next();
-});
+// var express = require('express');
+// var app = express();
+// var server = require('http').Server(app);
+// var io = require('socket.io')(server);
+//
+// // Socket server
+// var imageStream = io
+//     .of('/image-stream')
+//     .on('connection', function (socket) {
+//       socket.emit('news', { hello: 'world' });
+//       socket.on('my other event', function (data) {
+//         console.log(data);
+//       });
+//     });
+//
+// exports = module.exports = server;
+//
+// // marker for `grunt-express` to inject static folder/contents
+// server.use = function(){
+//     app.use.apply(app, arguments);
+// };
+// app.use(function staticsPlaceholder(req, res, next) {
+//   return next();
+// });
